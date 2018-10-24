abfb7f09242d7124daad32af46501f3ea7da00d2
Minewtech <375747625@qq.com>
2018-10-24 11:09:15 +0800

    Initial Commit

---
+++ VideoRecordAndPlayDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoRecordAndPlayDemo
+//
+//  Created by Minewtech on 2018/10/24.
+//  Copyright © 2018 Minewtech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
